c889d28549c0b4d0941d8e6b8eaf1b3c656f44aa
ျပည့္စံုဟိန္း <pyaesonehein1141991@gmail.com>
2020-02-10 10:56:49 +0630

    format code

--- src/main/java/murraco/controller/StudentLifeController.java
+++ src/main/java/murraco/controller/StudentLifeController.java
@@ -1,20 +1,14 @@
 package murraco.controller;
 
-
-
-
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.validation.Valid;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -30,40 +24,38 @@ import murraco.dto.StudentLifeProposalInsuredPersonDTO;
 @RequestMapping("/studentlife")
 @Api(tags = "Student_Life")
 public class StudentLifeController {
-	
-	@Autowired
-	private ModelMapper modelMapper;
+
+  @Autowired
+  private ModelMapper modelMapper;
+
+  @PostMapping("/submitproposal")
+  @ApiOperation(value = "${StudentLifeController.submitproposal}")
+  @ApiResponses(value = {@ApiResponse(code = 400, message = "Something went wrong"),
+      @ApiResponse(code = 403, message = "Access denied"),
+      @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+  public ResponseDTO<Object> submitproposal(
+      @Valid @RequestBody StudentLifeProposalDTO studentLifeProposalDTO) {
+
+    ResponseListDTO dto = new ResponseListDTO();
+    List<ResponseListDTO> responseList = new ArrayList<>();
+    for (StudentLifeProposalInsuredPersonDTO insuredPerson : studentLifeProposalDTO
+        .getProposalInsuredPersonList()) {
+      dto.setBpmsInsuredPersonId(insuredPerson.getBpmsInsuredPersonId());
+      dto.setPolicyNo("S/1904/0000000006");
+      dto.setProposalNo("SP/1904/0000000006");
+      if (insuredPerson.getCustomerID().equals(null) || insuredPerson.getCustomerID().isEmpty()) {
+        dto.setCustomerId("CUS111");
+      }
+      responseList.add(dto);
+    }
+    ResponseDTO<Object> responseDTO =
+        ResponseDTO.builder().responseStatus("Success!").responseBody(responseList).build();
+
+    return responseDTO;
+
+  }
 
 
-	@PostMapping("/submitproposal")
-	@ApiOperation(value = "${StudentLifeController.submitproposal}")
-	@ApiResponses(value = { 
-			@ApiResponse(code = 400, message = "Something went wrong"), 
-			@ApiResponse(code = 403, message = "Access denied"), 
-			@ApiResponse(code = 500, message = "Expired or invalid JWT token") })
-	public ResponseDTO<Object> submitproposal(@Valid @RequestBody StudentLifeProposalDTO studentLifeProposalDTO) {
 
-		ResponseListDTO dto = new ResponseListDTO();
-		List<ResponseListDTO> responseList = new ArrayList<>();
-		for(StudentLifeProposalInsuredPersonDTO insuredPerson:studentLifeProposalDTO.getProposalInsuredPersonList()) {
-			dto.setBpmsInsuredPersonId(insuredPerson.getBpmsInsuredPersonId());
-			dto.setPolicyNo("S/1904/0000000006");
-			dto.setProposalNo("SP/1904/0000000006");
-			if(insuredPerson.getCustomerID().equals(null) || insuredPerson.getCustomerID().isEmpty()) {
-				dto.setCustomerId("CUS111");
-			}
-			responseList.add(dto);
-		}
-		ResponseDTO<Object> responseDTO = ResponseDTO.builder()
-				.responseStatus("Success!")
-				.responseBody(responseList).build();
-		
-		return responseDTO;
-
-	}
-
-	
-
-	
 }
 
